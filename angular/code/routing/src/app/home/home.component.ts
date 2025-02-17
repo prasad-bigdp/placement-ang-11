1889c620a14f19e1caa92e24c1853147709e2176
prasad-bigdp <bigdp585@gmail.com>
2025-02-17 12:38:47 +0530

    update

---
+++ angular/code/routing/src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import '@dotlottie/player-component';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
